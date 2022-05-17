b99b4d8ab66d7150d20ca866ee398fbe2bb39f4f
tutv <tutv@foobla.com>
2022-05-17 16:23:43 +0700

    tset

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello world!")
+const express = require('express')
+
+const app = express()
+
+app.all("*", (req, res) => {
+    res.send(`URL: ${req.originalUrl}`)
+})
+
+app.listen(3001, () => {
+    console.log('app is listening on 3001')
+})
+

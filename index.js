034f41fc169010b06c34e3fb5724471e63a8c4aa
tutv <tutv@foobla.com>
2022-05-17 16:50:28 +0700

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-app.get('/ping', (req, res) => res.send('pong'))
+app.get('/ping', (req, res) => res.send('!!pong'))
 
 app.all("*", (req, res) => {
     res.send(`URL: ${req.originalUrl}`)
